ac76745865a9fb08747a47d72c8ddda2c9984b57
Paarshva Chitaliya <paarshva111@gmail.com>
2023-09-16 11:03:55 +0530

    final commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   double deviceHeight = 803.6363636363636;
   double deviceWidth = 392.72727272727275;
-  String url = 'https://7989-35-236-195-116.ngrok-free.app';
+  String url = 'https://6325-34-125-140-158.ngrok-free.app';
 }
