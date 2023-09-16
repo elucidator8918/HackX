555c3624fb9c9ed10fccc4710d69d9c40622e8c5
Paarshva Chitaliya <paarshva111@gmail.com>
2023-09-16 16:33:01 +0530

    last commit (real)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   double deviceHeight = 803.6363636363636;
   double deviceWidth = 392.72727272727275;
-  String url = 'https://ebab-34-125-140-158.ngrok-free.app';
+  String url = 'https://9788-34-125-140-158.ngrok-free.app';
 }
